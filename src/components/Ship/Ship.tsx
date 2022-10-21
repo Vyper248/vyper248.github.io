45e715fe3e740a2bb8a368d986326cbb207d3f3c
Christopher T Wilson <vyper374@hotmail.com>
2022-10-21 13:08:10 +0100

    Added optimisations to Ship, to stop main useEffect from running when ship isn't moving.

--- src/components/Ship/Ship.tsx
+++ src/components/Ship/Ship.tsx
@@ -1,15 +1,17 @@
-import { useEffect, useState, createRef, RefObject } from "react";
+import { useEffect, useState, createRef, RefObject, useCallback } from "react";
 
 import StyledShip from "./Ship.style";
 
 import { FRAME_SPEED, SPACE_WIDTH, SPACE_HEIGHT } from "../../utils/constants";
 import { PlanetProps as Planet } from "../Planet/Planet";
 
+import { cleanParticleArr } from "../Person/Person.utils";
+import { checkDistanceToPlanets, thrusterVelocity, bottomOfShip, direction } from "./Ship.utils";
+
 import Particle from "../Particle/Particle";
 import Label from "../Label/Label";
 
 const ROTATION_SPEED = 10*FRAME_SPEED/30;
-const MOVE_SPEED = 0.2*FRAME_SPEED/30;
 const THRUSTER_SPEED = 30;
 const MAX_SPEED = 5;
 const ALLOWABLE_KEYS = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'Enter'];
@@ -34,35 +36,20 @@ type ShipProps = {
     startingShipPos: ShipPos;
 }
 
-const direction = (rotation: number, type: 'forward' | 'backward'): [number, number] => {
-    let rotationAdjust = type === 'forward' ? -90 : 90;
-    let rotationRad = (rotation+rotationAdjust) * Math.PI / 180;
-    let yAdjust = MOVE_SPEED * Math.sin(rotationRad);
-    let xAdjust = MOVE_SPEED * Math.cos(rotationRad);
-    return [xAdjust, yAdjust];
-}
+const addParticle = (xPos: number, yPos: number, rotation: number, setParticleArr: React.Dispatch<React.SetStateAction<ParticleObj[]>>) => {
+    let key = Math.random()*200000;
+    let [x, y] = bottomOfShip(rotation);
+    let [xVel, yVel] = thrusterVelocity(rotation);
 
-const bottomOfShip = (rotation: number): [number, number] => {
-    let positionAdjust = (Math.random() * 8) - 4;
-    let rotationRad = (rotation+90) * Math.PI / 180;
-    let yAdjust = 30 * Math.sin(rotationRad) + 19 + positionAdjust;
-    let xAdjust = 30 * Math.cos(rotationRad) + 14 + positionAdjust;
-    return [xAdjust, yAdjust];
-}
+    let ref: RefObject<HTMLDivElement> = createRef();
+    let obj = {
+        particle: <Particle ref={ref} key={key} ixPos={xPos+x} iyPos={yPos+y} xVel={xVel*THRUSTER_SPEED} yVel={yVel*THRUSTER_SPEED} rotation={rotation}/>,
+        ref: ref
+    }
 
-const thrusterVelocity = (rotation: number): [number, number] => {
-    let rotationAdjust = (Math.random() * 50) - 20;
-    let rotationRad = (rotation+90+rotationAdjust) * Math.PI / 180;
-    let yAdjust = MOVE_SPEED * Math.sin(rotationRad);
-    let xAdjust = MOVE_SPEED * Math.cos(rotationRad);
-    return [xAdjust, yAdjust];
-}
-
-const distanceFromAToB = (a: [number, number], b: [number, number]): number => {
-    let xDiff = b[0] - a[0];
-    let yDiff = b[1] - a[1];
-    let distance = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
-    return distance;
+    setParticleArr(arr => {
+        return [...arr, obj];
+    });
 }
 
 const Ship = ({planets, startingShipPos}: ShipProps) => {
@@ -80,99 +67,16 @@ const Ship = ({planets, startingShipPos}: ShipProps) => {
         if (startingShipPos.x !== 0) setXPos(startingShipPos.x);
         if (startingShipPos.y !== 0) setYPos(startingShipPos.y);
         if (startingShipPos.r !== 0) setRotation(startingShipPos.r);
+        // eslint-disable-next-line
     }, []);
 
-    const addParticle = () => {
-        let key = Math.random()*200000;
-        let [x, y] = bottomOfShip(rotation);
-        let [xVel, yVel] = thrusterVelocity(rotation);
-
-        let ref: RefObject<HTMLDivElement> = createRef();
-        let obj = {
-            particle: <Particle ref={ref} key={key} ixPos={xPos+x} iyPos={yPos+y} xVel={xVel*THRUSTER_SPEED} yVel={yVel*THRUSTER_SPEED} rotation={rotation}/>,
-            ref: ref
-        }
-
-        setParticleArr(arr => {
-            return [...arr, obj];
-        });
-    }
-
-    //remove particles that have faded out
-    const cleanParticles = () => {
-        let firstParticle = particleArr[0];
-        if (!firstParticle) return;
-
-        let current = firstParticle.ref.current;
-        if (!current) return;
-
-        let opacity = parseFloat(current.style?.opacity);
-        if (opacity <= 0.2) {
-            setParticleArr(arr => {
-                arr.shift();
-                return arr;
-            });
-        }
-    }
-
-    const checkDistanceToPlanets = () => {
-        for (let i = 0; i < planets.length; i++) {
-            let { x, y, size } = planets[i];
-            let distance = distanceFromAToB([xPos, yPos], [x+size/2, y+size/2]);
-            if (distance < size/2) {
-                setCloseTo(planets[i]);
-                return;
-            }
-        }
-        setCloseTo({} as Planet);
-    }
-
-    // console.log(window.innerHeight/window.devicePixelRatio, window.innerWidth/window.devicePixelRatio);
-
-    const everyFrame = () => {
-        cleanParticles();
-        checkDistanceToPlanets();
-        let windowHeight = window.innerHeight;
-        let windowWidth = window.innerWidth;
-        // window.scrollTo(xPos-document.body.clientWidth/2, yPos-document.body.clientHeight/2);
-        window.scrollTo(xPos-windowWidth/2, yPos-windowHeight/2);
-
-        setXPos(xPos => {
-            if (xPos < -20) return SPACE_WIDTH+20 + xVel;
-            if (xPos > SPACE_WIDTH+20) return -20 + xVel;
-            return xPos + xVel;
-        });
-        setYPos(yPos => {
-            if (yPos < -20) return SPACE_HEIGHT+20 + yVel;
-            if (yPos > SPACE_HEIGHT+20) return -20 + yVel;
-            return yPos + yVel;
-        });
-
+    const manageKeyPresses = useCallback(() => {
         if (keyPresses['ArrowRight']) {
             setRotation(rotation => rotation+ROTATION_SPEED);
         }
 
         if (keyPresses['ArrowLeft']) {
             setRotation(rotation => rotation-ROTATION_SPEED);
-        }
-
-        if (keyPresses['ArrowUp']) {
-            let [xAdjust, yAdjust] = direction(rotation, 'forward');
-            addParticle();
-
-            //make sure velocity doesn't exceed max value
-            let adjusted = xVel+xAdjust > MAX_SPEED || xVel+xAdjust < -MAX_SPEED || yVel+yAdjust > MAX_SPEED || yVel+yAdjust < -MAX_SPEED;
-
-            if (xVel > MAX_SPEED) setXVel(MAX_SPEED);
-            if (xVel < -MAX_SPEED) setXVel(-MAX_SPEED);
-            if (yVel > MAX_SPEED) setYVel(MAX_SPEED);
-            if (yVel < -MAX_SPEED) setYVel(-MAX_SPEED);
-
-            //if it does, don't increase it.
-            if (adjusted) return;
-
-            setXVel(xVel => xVel + xAdjust);
-            setYVel(yVel => yVel + yAdjust);
         }
 
         if (keyPresses['ArrowDown']) {
@@ -188,13 +92,67 @@ const Ship = ({planets, startingShipPos}: ShipProps) => {
                 else return yVel-(yVel*0.1);
             });
         }
-    }
 
-    const keyDownListener = (e: KeyboardEvent) => {
+        if (keyPresses['ArrowUp']) {
+            let [xAdjust, yAdjust] = direction(rotation, 'forward');
+            addParticle(xPos, yPos, rotation, setParticleArr);
+
+            //make sure velocity doesn't exceed max value
+            let adjusted = xVel+xAdjust > MAX_SPEED || xVel+xAdjust < -MAX_SPEED || yVel+yAdjust > MAX_SPEED || yVel+yAdjust < -MAX_SPEED;
+
+            if (xVel > MAX_SPEED) setXVel(MAX_SPEED);
+            if (xVel < -MAX_SPEED) setXVel(-MAX_SPEED);
+            if (yVel > MAX_SPEED) setYVel(MAX_SPEED);
+            if (yVel < -MAX_SPEED) setYVel(-MAX_SPEED);
+
+            //if it does, don't increase it.
+            if (adjusted) return;
+
+            setXVel(xVel => xVel + xAdjust);
+            setYVel(yVel => yVel + yAdjust);
+        }
+    }, [rotation, xPos, yPos, xVel, yVel, setParticleArr, keyPresses]);
+
+    const everyFrame = useCallback(() => {
+        if (particleArr.length > 0) cleanParticleArr(particleArr, setParticleArr);
+
+        let windowHeight = window.innerHeight;
+        let windowWidth = window.innerWidth;
+        window.scrollTo(xPos-windowWidth/2, yPos-windowHeight/2);
+        
+        manageKeyPresses();
+        
+        if (xVel === 0 && yVel === 0) return;
+
+        let closeToPlanet = checkDistanceToPlanets(planets, xPos, yPos, closeTo);
+        if (closeToPlanet) setCloseTo(closeToPlanet);
+
+        setXPos(xPos => {
+            if (xPos < -20) return SPACE_WIDTH+20 + xVel;
+            if (xPos > SPACE_WIDTH+20) return -20 + xVel;
+            return xPos + xVel;
+        });
+        setYPos(yPos => {
+            if (yPos < -20) return SPACE_HEIGHT+20 + yVel;
+            if (yPos > SPACE_HEIGHT+20) return -20 + yVel;
+            return yPos + yVel;
+        });
+    }, [xVel, yVel, xPos, yPos, particleArr, manageKeyPresses, planets, closeTo]);
+
+    const keyDownListener = useCallback((e: KeyboardEvent) => {
         if (ALLOWABLE_KEYS.includes(e.code)) e.preventDefault();
 
         setKeyPresses(keyPresses => {
             keyPresses[e.code] = true;
+            return keyPresses;
+        });
+    }, []);
+
+    const keyUpListener = useCallback((e: KeyboardEvent) => {
+        if (ALLOWABLE_KEYS.includes(e.code)) e.preventDefault();
+        
+        setKeyPresses(keyPresses => {
+            keyPresses[e.code] = false;
             return keyPresses;
         });
 
@@ -203,16 +161,7 @@ const Ship = ({planets, startingShipPos}: ShipProps) => {
                 closeTo.onVisit({x: xPos, y: yPos, r: rotation});
             }
         }
-    }
-
-    const keyUpListener = (e: KeyboardEvent) => {
-        if (ALLOWABLE_KEYS.includes(e.code)) e.preventDefault();
-        
-        setKeyPresses(keyPresses => {
-            keyPresses[e.code] = false;
-            return keyPresses;
-        });
-    }
+    }, [closeTo, xPos, yPos, rotation]);
 
     useEffect(() => {
         window.addEventListener('keydown', keyDownListener);
@@ -225,7 +174,7 @@ const Ship = ({planets, startingShipPos}: ShipProps) => {
             window.removeEventListener('keyup', keyUpListener);
             clearInterval(interval);
         }
-    }, [rotation, xVel, yVel, particleArr, everyFrame]);
+    }, [everyFrame, keyDownListener, keyUpListener]);
 
     return (
         <>
---
+++ src/components/Ship/Ship.utils.ts
@@ -0,0 +1,50 @@
+import { PlanetProps as Planet } from "../Planet/Planet";
+
+import { FRAME_SPEED } from "../../utils/constants";
+
+const MOVE_SPEED = 0.2*FRAME_SPEED/30;
+
+export const direction = (rotation: number, type: 'forward' | 'backward'): [number, number] => {
+    let rotationAdjust = type === 'forward' ? -90 : 90;
+    let rotationRad = (rotation+rotationAdjust) * Math.PI / 180;
+    let yAdjust = MOVE_SPEED * Math.sin(rotationRad);
+    let xAdjust = MOVE_SPEED * Math.cos(rotationRad);
+    return [xAdjust, yAdjust];
+}
+
+export const bottomOfShip = (rotation: number): [number, number] => {
+    let positionAdjust = (Math.random() * 8) - 4;
+    let rotationRad = (rotation+90) * Math.PI / 180;
+    let yAdjust = 30 * Math.sin(rotationRad) + 19 + positionAdjust;
+    let xAdjust = 30 * Math.cos(rotationRad) + 14 + positionAdjust;
+    return [xAdjust, yAdjust];
+}
+
+export const thrusterVelocity = (rotation: number): [number, number] => {
+    let rotationAdjust = (Math.random() * 50) - 20;
+    let rotationRad = (rotation+90+rotationAdjust) * Math.PI / 180;
+    let yAdjust = MOVE_SPEED * Math.sin(rotationRad);
+    let xAdjust = MOVE_SPEED * Math.cos(rotationRad);
+    return [xAdjust, yAdjust];
+}
+
+const distanceFromAToB = (a: [number, number], b: [number, number]): number => {
+    let xDiff = b[0] - a[0];
+    let yDiff = b[1] - a[1];
+    let distance = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
+    return distance;
+}
+
+export const checkDistanceToPlanets = (planets: Planet[], xPos: number, yPos: number, closeTo: Planet): Planet | null => {
+    for (let i = 0; i < planets.length; i++) {
+        let { x, y, size } = planets[i];
+        let distance = distanceFromAToB([xPos, yPos], [x+size/2, y+size/2]);
+        if (distance < size/2) {
+            return planets[i];
+        }
+    }
+
+    if (Object.values(closeTo).length === 0) return null;
+
+    return {} as Planet;
+}
